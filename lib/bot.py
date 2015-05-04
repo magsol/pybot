8dac72a6219dd7d85d86eab8ba1b2af595127166
Shannon Quinn <magsol@gmail.com>
2015-05-04 17:05:15 -0400

    Filled out custom callbacks and signal handling, as well as the bulk of the main run method. Still need to figure out how to implement the DSL before the remainder of the the run method can be completed.

--- lib/bot.py
+++ lib/bot.py
@@ -13,6 +13,7 @@ limitations under the License.
 """
 
 import logging
+import signal
 import time
 
 import tweepy
@@ -35,7 +36,8 @@ class PyBot(object):
 
         # Action intervals. Set any of these to 0 to disable them entirely.
         # Otherwise, the integer indicates the number of seconds between
-        # heartbeats.
+        # heartbeats. Alternatively, these can be callables that return
+        # some number (integer) of seconds.
         self.config['timeline_interval'] = 0
         self.config['mention_interval'] = 0
         self.config['search_interval'] = 0
@@ -77,6 +79,9 @@ class PyBot(object):
         # Now we start initializing the bot.
         #
 
+        # Set up a signal handler so a bot can gracefully exit.
+        signal.signal(signal.SIGINT, self._handler)
+
         # Set up logging.
         logging.basicConfig(format = '%(asctime)s | %(levelname)s: %(message)s',
             datefmt = '%m/%d/%Y %I:%M:%S %p',
@@ -103,27 +108,34 @@ class PyBot(object):
             # No previous state to load? Initialize everything.
             self.state = {}
 
+            # Wait time for the next action.
+            self.state['next_action'] = 0
+
             # Timeline configuration options. Set timeline_interval to 0 to
             # disable checking the bot's timeline.
             self.state['last_timeline_id'] = 1
             self.state['last_timeline_time'] = 0
+            self.state['next_timeline_time'] = 0
             self.state['recent_timeline'] = []
 
             # Mention configuration options. Set mention_interval to 0 to
             # disable checking the bot's mentions.
             self.state['last_mention_id'] = 1
             self.state['last_mention_time'] = 0
+            self.state['next_mention_time'] = 0
             self.state['recent_mentions'] = []
 
             # Keyword search configuration options. Set search_interval to 0 to
             # disable searching for keywords in the public timeline.
             self.state['last_search_id'] = 1
-            self.state['last_search_time'] = 1
+            self.state['last_search_time'] = 0
+            self.state['next_search_time'] = 0
 
             # Active tweeting configuration. Set tweet_interval to 0 to
             # disable posting otherwise-unprovoked tweets.
             self.state['last_tweet_id'] = 1
             self.state['last_tweet_time'] = 0
+            self.state['next_tweet_time'] = 0
 
             # List of user IDs you follow.
             self.state['friends'] = self.api.friends_ids(self.id)
@@ -134,20 +146,9 @@ class PyBot(object):
             # List of new followers since the last check (internal) timestamp.
             self.state['new_followers'] = []
             self.state['last_follow_time'] = time.time()
+            self.state['next_follow_time'] = 0
 
         logging.info("Bot state set.")
-
-    def bot_init(self):
-        """
-        Initializes all bot-specific configuration.
-        """
-        raise NotImplementedError("D'oh, you didn't implement the 'bot_init()' method.")
-
-    def run(self):
-        """
-        PyBot's main run method. This activates ALL the things.
-        """
-        pass
 
     def register_custom_callback(self, action, condition, interval):
         """
@@ -160,14 +161,16 @@ class PyBot(object):
             Function which specifies the custom action to take.
         condition : function
             Function which evaluates to True or False.
-        interval : integer
-            Number of seconds to wait before checking the condition.
+        interval : integer or callable
+            Number of seconds to wait before checking the condition, or a
+            callable that returns the number of seconds to wait.
         """
         callback = {
             'action': action,
             'condition': condition,
             'interval': interval,
             'last_run': 0,
+            'next_run': 0,
         }
         self.custom_callbacks.append(callback)
 
@@ -180,8 +183,135 @@ class PyBot(object):
     def on_timeline(self):
         raise NotImplementedError("Need to implement (or pass) 'on_timeline'.")
 
-    def on_follow(self):
-        raise NotImplementedError("Need to implement (or pass) 'on_follow'.")
+    def on_follow(self, f_id):
+        """
+        Handler when a new follower is / new followers are detected.
+
+        For basic uses, the configuration options provided will suffice. If you
+        also want the bot to do other things when a new follower is acquired, you
+        will need to override this method (though you can avoid replicating the
+        code here by invoking super.on_follow()).
+
+        Parameters
+        ----------
+        f_id : string
+            Twitter user ID of the new follower.
+        """
+        self.state['followers'].append(f_id)
+
+        # Do we automatically follow back?
+        if self.config['autofollow']:
+            try:
+                self.api.create_friendship(f_id, follow = True)
+                self.state['friends'].append(f_id)
+                logging.info("Followed user id '%s'" % f_id)
+            except tweepy.TweepyError as e:
+                logging.error("Error following user '%s': %s, %s" % (f_id, e.message[0]['code'], e.message[0]['message']))
 
     def on_search(self):
         raise NotImplementedError("Need to implement (or pass) 'on_search'.")
+
+    def bot_init(self):
+        """
+        Custom initialization. Specify any configuration options you want to
+        override, as in particular your OAuth credentials.
+
+        ** Use the provided scripts to generate a template for you to fill in. **
+        """
+        raise NotImplementedError("D'oh, you didn't implement the 'bot_init()' method.")
+
+    def run(self):
+        """
+        PyBot's main run method. This activates ALL the things.
+        """
+        self.running = True
+        while self.running:
+            intervals = []
+            current_time = time.time()
+
+            # Check followers.
+            if self.config['follow_interval'] != 0 and \
+                    current_time > self.state['next_follow_time']:
+
+                # Update.
+                self.state['last_follow_time'] = current_time
+                self.state['next_follow_time'] = self._increment(current_time, self.config['follow_interval'])
+
+            # Check timeline.
+            if self.config['timeline_interval'] != 0 and \
+                    current_time > self.state['next_timeline_time']:
+
+                # Update.
+                self.state['last_timeline_time'] = current_time
+                self.state['next_timeline_time'] = self._increment(current_time, self.config['timeline_interval'])
+
+            # Check mentions.
+            if self.config['mention_interval'] != 0 and \
+                    current_time > self.state['next_mention_time']:
+
+                # Update.
+                self.state['last_mention_time'] = current_time
+                self.state['next_mention_time'] = self._increment(current_time, self.config['mention_interval'])
+
+            # Check keywords.
+            if self.config['search_interval'] != 0 and \
+                    current_time > self.state['next_search_time']:
+
+                # Update.
+                self.state['last_search_time'] = current_time
+                self.state['next_search_time'] = self._increment(current_time, self.config['search_interval'])
+
+            # Check tweets.
+            if self.config['tweet_interval'] != 0 and \
+                    current_time > self.state['next_tweet_time']:
+
+                # Update.
+                self.state['last_tweet_time'] = current_time
+                self.state['next_tweet_time'] = self._increment(current_time, self.config['tweet_interval'])
+
+            # Check custom handlers.
+            for callback in self.custom_callbacks:
+                if callback['condition']() and current_time > callback['next_run']:
+                    # Invoke the action!
+                    callback['action']()
+
+                    # Update the interval for the next run.
+                    callback['last_run'] = current_time
+                    callback['next_run'] = self._increment(current_time, callback['interval'])
+                    intervals.append(callback['next_run'])
+
+            # Save the current state.
+            self._save_state()
+
+            # Find the next timestamp.
+            next_action = min(intervals)
+            if current_time < next_action:
+                time.sleep(next_action - current_time)
+
+        # If the loop breaks, someone hit CTRL+C.
+        logging.info("Shutdown complete!")
+
+    def _increment(self, previous, interval):
+        """
+        Helper method for dealing with callable time intervals.
+        """
+        update = previous
+        if hasattr(interval, '__call__'):
+            update += interval()
+        else:
+            update += interval
+        return update
+
+    def _handler(self, signum, frame):
+        """
+        Signal handler. Gracefully exits.
+        """
+        logging.info("SIGINT caught, beginning shutdown...")
+        self.running = False
+
+    def _save_state(self):
+        """
+        Serializes the current bot's state in case we halt.
+        """
+        self.config['storage'].write('%s_state.pkl' % self.screen_name, self.state)
+        logging.info("Bot state saved.")
